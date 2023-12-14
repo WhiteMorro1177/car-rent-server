641af20b1af969bf4d71654d275c612c54dbe74f
WhiteMorro1177 <jackforest228@gmail.com>
2023-12-14 15:48:34 +0300

    edited rest server

--- index.js
+++ index.js
@@ -18,36 +18,70 @@ const app = express();
 
 // login controller
 app.get("/login/:username&:password", async (req, res) => {
-    const inputUsername = req.params.username;
-    const inputPassword = req.params.password;
+    const username = req.params.username;
+    const password = req.params.password;
 
-    const user_role = await client.query(`call check_auth(${inputUsername})`);
+    const queryResult = await client.query("select check_auth($1, $2)", [username, password]);
+    const user_role = queryResult.rows[0].check_auth;
 
-    console,log(user_role);
-    if (user_role === undefined) {
-        res.status(404).send("Account does not exists");
+    console.log(user_role);
+    if (user_role === "null") {
+        res.status(404).send(null);
     }
-    
-    const password = await client.query("select rolpassword from pg_roles where rolname = $1", [user_role]);
-    
-    console,log(password);
-    if (inputPassword !== password) {
-        res.status(404).send("Wrong password");
-    }
+    console.log("Authentification successfull");
+    res.status(200).send(user_role);
+});
 
-    res.status(200).send("ok");
+app.post("/auth/:username&:password&:role", async (req, res) => {
+    const username = req.params.username;
+    const password = req.params.password;
+    const role = req.params.role;
+
+    await client.query("call add_user($1, $2, $3)", [username, password, role]);
+    res.status(200).send(role);
 });
 
 
 // client controller
 
+// get cars list
+app.get("/cars/:username", async (req, res) => {
+    const username = req.params.username;
+    await client.query("set role $1", [username]);
+    const queryResult = await client.query("select * from get_cars()");
+    console.log(queryResult);
+    res.status(200).send("ok");
+});
+
+// get parkings list
+app.get("/parkings", async (req, res) => {
+    res.status(200).send("ok");
+});
+
+// create contract
+app.post("/contract/:users/:vin&:duration", async (req, res) => {
+    const username = req.params.username;
+    await client.query("set role $1", [username]);
+
+    // const drivingLisence = req.params.lisenceId;
+    const vinNumber = req.params.vin;
+    const rentalDuration = req.params.duration;
+
+    const queryResult = await client.query("select prepare_contract($1, $2, $3)", [username, vinNumber, rentalDuration]);
+    console.log(queryResult);
+    res.status(200).send("ok");
+});
+
 
 
 // manager controller
-app.get("/contract/:id", async (req, res) => {
-    
+
+// sign contract
+app.post("/contract/:id", async (req, res) => {
     res.status(200).send("ok")
-})
+});
+
+// 
 
 
 // admin controller
