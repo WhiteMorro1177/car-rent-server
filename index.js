33143448260a4eb0189db83eac9c3fb88d45a624
WhiteMorro1177 <jackforest228@gmail.com>
2023-12-21 22:26:12 +0300

    updated index.js

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const client = new pg.Client({
   })
 
 
-const PORT = process.env.PORT | 3002;
+const PORT = process.env.PORT || 3002;
+const errorMessage = "Error in request";
 
 import express from "express";
 const app = express();
@@ -18,26 +19,34 @@ const app = express();
 
 // login controller
 app.get("/login/:username&:password", async (req, res) => {
-    const username = req.params.username;
-    const password = req.params.password;
-
-    const queryResult = await client.query("select check_auth($1, $2)", [username, password]);
-    const user_role = queryResult.rows[0].check_auth;
-
-    console.log(user_role);
-    if (user_role === "null") {
-        res.status(404).send(null);
+    try {
+        const username = req.params.username;
+        const password = req.params.password;
+        
+        const queryResult = await client.query("select check_auth($1, $2)", [username, password]);
+        const user_role = queryResult.rows[0].check_auth;
+        
+        console.log(user_role);
+        if (user_role === "null") {
+            res.status(404).send(null);
+        }
+        console.log("Authentification successfull");
+        res.status(200).send(user_role);
+    } catch (exc) {
+        res.status(404).send(errorMessage);
     }
-    console.log("Authentification successfull");
-    res.status(200).send(user_role);
 });
 
-app.post("/auth/:username&:password&:role", async (req, res) => {
-    const username = req.params.username;
-    const password = req.params.password;
-
-    await client.query("call add_user($1, $2)", [username, password]);
-    res.status(200).send(username);
+app.post("/auth/:username&:password", async (req, res) => {
+    try {
+        const username = req.params.username;
+        const password = req.params.password;
+        
+        await client.query("call add_user($1, $2)", [username, password]);
+        res.status(200).send(username);
+    } catch (exc) {
+        res.status(404).send(errorMessage);
+    }
 });
 
 
@@ -45,12 +54,16 @@ app.post("/auth/:username&:password&:role", async (req, res) => {
 
 // get cars list
 app.get("/cars/:username", async (req, res) => {
-    const username = req.params.username;
-    await client.query("set role $1", [username]);
-
-    const queryResult = await client.query("select * from get_cars()");
-    console.log(queryResult); // !!! parse the result !!!
-    res.status(200).send("ok");
+    try {
+        const username = req.params.username;
+        await client.query(`set role ${username}`);
+        
+        const queryResult = await client.query("select * from get_cars()");
+        console.log(queryResult); // !!! parse the result !!!
+        res.status(200).send(queryResult.rows);
+    } catch (exc) {
+        res.status(404).send(errorMessage);
+    }
 });
 
 // get parkings list
@@ -60,22 +73,26 @@ app.get("/parkings", async (req, res) => {
 
 // create contract
 app.post("/contract/:username/:vin&:duration", async (req, res) => {
-    const username = req.params.username;
-    await client.query("set role $1", [username]);
+    try {
+        const username = req.params.username;
+        await client.query(`set role ${username}`);
 
-    // const drivingLisence = req.params.lisenceId;
-    const vinNumber = req.params.vin;
-    const rentalDuration = req.params.duration;
+        // const drivingLisence = req.params.lisenceId;
+        const vinNumber = req.params.vin;
+        const rentalDuration = req.params.duration;
 
-    const queryResult = await client.query("select prepare_contract($1, $2, $3)", [username, vinNumber, rentalDuration]);
-    console.log(queryResult); // !!! parse the result !!!
-    res.status(200).send("ok");
+        const queryResult = await client.query("select prepare_contract($1, $2, $3)", [username, vinNumber, rentalDuration]);
+        console.log(queryResult); // !!! parse the result !!!
+        res.status(200).send("ok");
+    } catch (exc) {
+        res.status(404).send(errorMessage);
+    }
 });
 
 // report about car accident
 app.post("/accident/:username&:vin", async (req, res) => {
     const username = req.params.username;
-    await client.query("set role $1", [username]);
+    await client.query(`set role ${username}`);
 
     const vin = req.params.vin;
     const queryResult = await client.query("select add_accident($1, $2)", [username, vin]);
